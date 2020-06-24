731bc148f4207ffcf5a4fb2359a48adcaa837102
Max <maxguojiaqi@gmail.com>
2020-06-24 10:56:22 -0400

    Updates

---
+++ precache-manifest.30b1e07f89cae5ac44761f8241663f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6856b4a594dc343e7088feffed8ac832",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "e92e9a2925887f07dc2a",
+    "url": "/Employee-Directory/static/css/main.dcb1441c.chunk.css"
+  },
+  {
+    "revision": "d29decf105d18e668ffa",
+    "url": "/Employee-Directory/static/js/2.c6eb980b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.c6eb980b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92e9a2925887f07dc2a",
+    "url": "/Employee-Directory/static/js/main.d45efeb9.chunk.js"
+  },
+  {
+    "revision": "52e1e8e23e7dfaf64514",
+    "url": "/Employee-Directory/static/js/runtime-main.6ca9f082.js"
+  }
+]);
